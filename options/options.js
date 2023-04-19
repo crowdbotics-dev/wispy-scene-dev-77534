d78d8ef6e756f99f426cbc4d5f09c9dee1623b23
Crowdbotics <engineering@crowdbotics.com>
2023-04-19 02:59:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-scene-dev-77534",
+  url: "https://wispy-scene-dev-77534.botics.co",
+  api: "https://wispy-scene-dev-77534.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
